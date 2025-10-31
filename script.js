20f1bca0df8d0e6c2bfca3d482d3404306400596
iolyab <iolyabru@gmail.com>
2025-10-31 16:01:21 -0400

    Refactored

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ const dice = document.querySelector(".dice");
 const rotateBtn = document.querySelector(".rotateBtn");
 
 let rotation = 0;
+let scaleValue = 1;
 
 range.addEventListener("input", () => {
-  const scaleValue = range.value / 100;
-  dice.style.transform = `scale(${scaleValue})`;
+  scaleValue = range.value / 100;
+  dice.style.transform = `scale(${scaleValue}) rotate(${rotation}deg)`;
 });
 
 rotateBtn.addEventListener("click", () => {
   rotation += 180;
-  dice.style.transform = `rotate(${rotation}deg)`;
+  dice.style.transform = `scale(${scaleValue}) rotate(${rotation}deg)`;
 });
